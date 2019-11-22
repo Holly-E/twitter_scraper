6c6fc80034b0b913c2efc8ed84c9a0ce86932c55
Holly-E <holly.erickson@icloud.com>
2019-11-22 15:34:42 -0500

    add server.py

--- bot.py
+++ bot.py
@@ -5,28 +5,11 @@ Created on Thu Nov 21 16:06:56 2019
 @author: Erick
 """
 
-#from generate_advertisement import get_ad
 from twython import Twython
 from twython import TwythonStreamer 
 #import json as json
 
 
-#from flask import Flask
-#from waitress import serve
-
-from os import environ
-from flask import Flask
-
-app = Flask(__name__)
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
-
-#app = Flask(__name__)
-#serve(app, listen='*:8080')
-#app.run()
-
-@app.route("/")
-def whatever_homepage_blah():
-    return "Helloo world!"
 
  
 """
---
+++ server.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 16:06:56 2019
+
+@author: Erick
+"""
+
+#from flask import Flask
+#from waitress import serve
+
+from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
+#app = Flask(__name__)
+#serve(app, listen='*:8080')
+#app.run()
